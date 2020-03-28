d526535f57c48b054d7a477c864933af3e5e0f12
Louis <lreillywillhight@gmail.com>
2020-03-28 02:40:00 -0700

    i have no idea what i'm doing

--- memory-game/memory-game/js/main.js
+++ memory-game/memory-game/js/main.js
@@ -1,10 +1,26 @@
 console.log("hello there.")
 //begin program
 
-let cardOne = "king";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "queen";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
 
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("you found a match!") 
+	}
+	else {
+		alert("Sorry, try again.")
+	}
+	}
+
+
+
+console.log(cardOne);
+console.log(cardTwo);
+console.log("User flipped " + cardsInPlay)
